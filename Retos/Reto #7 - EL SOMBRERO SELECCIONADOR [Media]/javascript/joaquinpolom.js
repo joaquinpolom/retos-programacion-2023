030d8356572b88bbaac9bab2e869f7f75856e87d
Joaquin Polo Molero <joaquin.polo.m@outlook.es>
2023-02-18 21:02:10 +0000

    Update joaquinpolom.js

--- Retos/Reto #7 - EL SOMBRERO SELECCIONADOR [Media]/javascript/joaquinpolom.js
+++ Retos/Reto #7 - EL SOMBRERO SELECCIONADOR [Media]/javascript/joaquinpolom.js
@@ -53,44 +53,101 @@
 // c) No conocer o aprender lo suficiente sobre el mundo (curiosidad)
 // d) Perder a alguien que amo o ser traicionado (lealtad)
 
-let questions = ["¿Cuál de estos rasgos es más importante para ti en un amigo?",
-                 "¿Cuál de estos libros te interesaría más leer?",
-                 "¿Cuál de estos deportes preferirías jugar?",
-                 "¿Qué opinas sobre probar nuevos alimentos o platos?",
-                 "¿Qué opinas sobre viajar a países desconocidos?",
-                 "¿Qué te motiva a realizar una tarea o proyecto?",
-                 "¿Cuál es tu mayor miedo?"];
-let answers = [["a) Coraje y valentía (osadía)",
-                "b) Ambición y éxito (ambición)",
-                "c) Inteligencia y curiosidad (curiosidad)",
-                "d) Honestidad y lealtad (lealtad)"],
-               ["a) Un libro sobre deportes extremos y aventuras (osadía)",
-                "b) Un libro sobre emprendimiento y negocios (ambición)",
-                "c) Un libro sobre ciencia o tecnología (curiosidad)",
-                "d) Un libro sobre relaciones y emociones humanas (lealtad)"],
-               ["a) Escalada en roca o paracaidismo (osadía)",
-                "b) Tenis o golf (ambición)",
-                "c) Esquí o snowboard (curiosidad)",
-                "d) Natación o ciclismo (lealtad)"],
-               ["a) Me encanta probar cosas nuevas y diferentes (osadía)",
-                "b) Solo pruebo cosas nuevas si tienen buena reputación (ambición)",
-                "c) Me gusta probar nuevos sabores y platos de diferentes culturas (curiosidad)",
-                "d) Me gusta comer lo que ya conozco y me gusta (lealtad)"],
-               ["a) Me emociona explorar nuevos lugares y culturas (osadía)",
-                "b) Solo viajaría a países seguros y con buena reputación (ambición)",
-                "c) Me encanta viajar y descubrir cosas nuevas (curiosidad)",
-                "d) Me gusta viajar a lugares conocidos o a los que he visitado antes (lealtad)"],
-               ["a) El desafío de hacer algo difícil (osadía)",
-                "b) La posibilidad de obtener una recompensa o logro (ambición)",
-                "c) El deseo de aprender algo nuevo (curiosidad)",
-                "d) La satisfacción de ayudar a alguien o ser útil (lealtad)"],
-               ["a) No ser lo suficientemente valiente en una situación difícil (osadía)",
-                "b) No tener éxito en la vida (ambición)",
-                "c) No conocer o aprender lo suficiente sobre el mundo (curiosidad)",
-                "d) Perder a alguien que amo o ser traicionado (lealtad)"]];
 
-for (var i in questions) {
-  console.log(questions[i]);
-  console.log(answers[i][1]);
+function Select_Hat() {
+  let questions = ["¿Cuál de estos rasgos es más importante para ti en un amigo?",
+                   "¿Cuál de estos libros te interesaría más leer?",
+                   "¿Cuál de estos deportes preferirías jugar?",
+                   "¿Qué opinas sobre probar nuevos alimentos o platos?",
+                   "¿Qué opinas sobre viajar a países desconocidos?",
+                   "¿Qué te motiva a realizar una tarea o proyecto?",
+                   "¿Cuál es tu mayor miedo?"];
+  let answers = [["a) Coraje y valentía (osadía)",
+                  "b) Ambición y éxito (ambición)",
+                  "c) Inteligencia y curiosidad (curiosidad)",
+                  "d) Honestidad y lealtad (lealtad)"],
+                 ["a) Un libro sobre deportes extremos y aventuras (osadía)",
+                  "b) Un libro sobre emprendimiento y negocios (ambición)",
+                  "c) Un libro sobre ciencia o tecnología (curiosidad)",
+                  "d) Un libro sobre relaciones y emociones humanas (lealtad)"],
+                 ["a) Escalada en roca o paracaidismo (osadía)",
+                  "b) Tenis o golf (ambición)",
+                  "c) Esquí o snowboard (curiosidad)",
+                  "d) Natación o ciclismo (lealtad)"],
+                 ["a) Me encanta probar cosas nuevas y diferentes (osadía)",
+                  "b) Solo pruebo cosas nuevas si tienen buena reputación (ambición)",
+                  "c) Me gusta probar nuevos sabores y platos de diferentes culturas (curiosidad)",
+                  "d) Me gusta comer lo que ya conozco y me gusta (lealtad)"],
+                 ["a) Me emociona explorar nuevos lugares y culturas (osadía)",
+                  "b) Solo viajaría a países seguros y con buena reputación (ambición)",
+                  "c) Me encanta viajar y descubrir cosas nuevas (curiosidad)",
+                  "d) Me gusta viajar a lugares conocidos o a los que he visitado antes (lealtad)"],
+                 ["a) El desafío de hacer algo difícil (osadía)",
+                  "b) La posibilidad de obtener una recompensa o logro (ambición)",
+                  "c) El deseo de aprender algo nuevo (curiosidad)",
+                  "d) La satisfacción de ayudar a alguien o ser útil (lealtad)"],
+                 ["a) No ser lo suficientemente valiente en una situación difícil (osadía)",
+                  "b) No tener éxito en la vida (ambición)",
+                  "c) No conocer o aprender lo suficiente sobre el mundo (curiosidad)",
+                  "d) Perder a alguien que amo o ser traicionado (lealtad)"]];
+  var osadia = 0;
+  var ambicion = 0;
+  var curiosidad = 0;
+  var lealtad = 0;
+
+  //const prompt = require('prompt-sync')();
+  const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  
+
+  for (var i in questions) {
+    console.log(questions[i]);
+    console.log(answers[i][0]);
+    console.log(answers[i][1]);
+    console.log(answers[i][2]);
+    console.log(answers[i][3]);
+    console.log("Respuesta: ");
+    console.log("\n");
+    //const answer = prompt("Respuesta: ");
+    readline.question("Respuesta: ", answer => {
+      console.log(answer);
+      readline.close();
+    });
+    
+
+    // Lee respuesta
+    //process.stdin.setRawMode(true);
+    process.stdin.on('readable', function () {
+      var answer = String(process.stdin.read());
+      console.log(answer);
+      switch (answer) {
+        case "a":
+          osadia += 1;
+          console.log("Osadia");
+          break;
+        case "b":
+          ambicion += 1;
+          console.log("Ambicion");
+          break;
+        case "c":
+          curiosidad += 1;
+          console.log("curiosidad");
+          break;
+        case "d":
+          lealtad += 1;
+          console.log("lealtad");
+          break;
+        default:
+          console.log("Respuesta incorrecta");
+          process.exit();
+          break;
+      }
+    });
+  };
 };
+
+Select_Hat();
+
 
